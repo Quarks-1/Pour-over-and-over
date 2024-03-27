aa7e74e7ed5de6d5a48932f1dd69b8d6ea3b42e9
Quarks01 <riopacheco7@gmail.com>
2024-03-27 10:22:16 -0400

    fix usb port

--- gcode_testing/test.py
+++ gcode_testing/test.py
@@ -7,5 +7,7 @@ time.sleep(2)
 for command in ["G28", "M114"]:
     ser.write(str.encode(command + "\r\n"))
     time.sleep(1)
-print(ser.read_all().decode())
+while True:
+     cc=str(ser.readline())
+     print(cc)
 ser.close()
