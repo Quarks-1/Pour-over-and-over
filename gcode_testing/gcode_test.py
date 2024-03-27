e6306dc44140b5a37c09fccdcd10411290c991ce
Quarks01 <riopacheco7@gmail.com>
2024-03-27 11:27:25 -0400

    add gcode

--- gcode_testing/gcode_test.py
+++ gcode_testing/gcode_test.py
@@ -4,7 +4,7 @@ import time
 ser = serial.Serial("/dev/ttyUSB0", 115200)
 time.sleep(2)
 # ser.write(str.encode("G28\r\n"))
-for command in ["G28 X Z", "M114", "G1 X127 Y90 Z220 F3600", "G2 I20 J20","M114"]:
+for command in ["G28 X Y", "M114", "G1 X127 Y90 Z220 F3600", "G2 I20 J20","M114"]:
     ser.write(str.encode(command + "\r\n"))
     time.sleep(1)
 while True:
