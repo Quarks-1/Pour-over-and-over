fc2a30052a8e3d5813986845436f90ff25e0a959
Quarks01 <riopacheco7@gmail.com>
2024-03-27 10:19:50 -0400

    fix usb port

--- gcode_testing/test.py
+++ gcode_testing/test.py
@@ -3,9 +3,9 @@ import time
 
 ser = serial.Serial("/dev/ttyUSB0", 115200)
 time.sleep(2)
-ser.write("G28\r\n")
-time.sleep(1)
-ser.write("M114\r\n")
-time.sleep(1)
+
+for command in ["G28", "M114"]:
+    ser.write(str.encode(command) + "\r\n")
+    time.sleep(1)
 print(ser.read_all().decode())
 ser.close()
