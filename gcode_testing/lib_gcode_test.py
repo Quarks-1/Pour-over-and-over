f02dc87e0e0f6da14f0ee9b0535ab472232db1dc
Quarks01 <riopacheco7@gmail.com>
2024-03-27 11:56:31 -0400

    add gcode lib file

--- gcode_testing/lib_gcode_test.py
+++ gcode_testing/lib_gcode_test.py
@@ -5,16 +5,17 @@ ser = serial.Serial("/dev/ttyUSB0", 115200)
 time.sleep(2)
 # ser.write(str.encode("G28\r\n"))
 command_dict = {
-    'home': 'G28 X Z',
-    'abs_center' : 'G1 X127 Y90 Z220 F3600',
-    'center' : 'G1 X127 Y90 F3600',
+    'home': 'G28 X Y',
+    'abs_center' : 'G0 X127 Y90 Z220 F3600',
+    'center' : 'G0 X127 Y90 F3600',
+    'pos': 'M114',
 }
 while True:
     input_command = input("Enter a GCODE command: ")
     if input_command in command_dict:
         input_command = command_dict[input_command]
     ser.write(str.encode(input_command + "\r\n"))
-    time.sleep(1)
+    time.sleep(2)
     cc=str(ser.readline())
     print(cc)
 ser.close()
