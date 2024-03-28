638c682da288009afca883c8667c973eebe007da
Quarks01 <riopacheco7@gmail.com>
2024-03-28 14:48:43 -0400

    improve lib_gcode_test, add recording feature

--- gcode_testing/lib_gcode_test.py
+++ gcode_testing/lib_gcode_test.py
@@ -10,12 +10,36 @@ command_dict = {
     'center' : 'G0 X127 Y90 F3600',
     'pos': 'M114',
 }
+recording = []
+record = False
 while True:
     input_command = input("Enter a GCODE command: ")
     if input_command in command_dict:
         input_command = command_dict[input_command]
+    elif input_command == "record":
+        record = not record
+        if record:
+            print("Recording...")
+        else:
+            print("Stopped recording.")
+    elif input_command == 'undo':
+        recording.pop()
+        continue
+    elif input_command == 'clear':
+        recording = []
+        continue
+    elif input_command == 'exit':
+        break
+    
+    if record:
+        recording.append(input_command)
+    
     ser.write(str.encode(input_command + "\r\n"))
     time.sleep(2)
     cc=str(ser.readline())
     print(cc)
+    
+
+print(f'Recording: {recording}')
+print("Exiting...")
 ser.close()
