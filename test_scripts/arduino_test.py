a4de0e4d5b10dcb1bf92db4381a0d71daa65c009
Quarks01 <riopacheco7@gmail.com>
2024-03-30 16:00:36 -0400

    modify test files

--- test_scripts/arduino_test.py
+++ test_scripts/arduino_test.py
@@ -3,6 +3,7 @@ import serial
 import time 
 arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1) 
 def write_read(x):
+    arduino.reset_input_buffer()
     arduino.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
     data = arduino.readline() 
