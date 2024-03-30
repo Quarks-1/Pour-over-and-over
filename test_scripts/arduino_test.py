bae3ce4f2c7f7b1587440467eef6dcf456a245f7
Quarks01 <riopacheco7@gmail.com>
2024-03-30 16:04:03 -0400

    modify test files

--- test_scripts/arduino_test.py
+++ test_scripts/arduino_test.py
@@ -2,13 +2,9 @@
 import serial 
 import time 
 arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1) 
-def write_read():
+
+while True:
     arduino.reset_input_buffer()
-    # arduino.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
     data = arduino.readline() 
-    return data 
-while True:
-    # num = input("Enter a number: ") # Taking input from user 
-    value = write_read() 
-    print(value) # printing the value 
+    print(data) # printing the value 
