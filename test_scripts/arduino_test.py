4ad6959f48ebee02510c02ddbabac104cd6b308c
Quarks01 <riopacheco7@gmail.com>
2024-03-30 16:24:33 -0400

    modify test files

--- test_scripts/arduino_test.py
+++ test_scripts/arduino_test.py
@@ -4,7 +4,7 @@ import time
 arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1) 
 
 while True:
-    arduino.reset_input_buffer()
+    # arduino.reset_input_buffer()
     time.sleep(0.05)
     data = arduino.readline() 
     print(data) # printing the value 
