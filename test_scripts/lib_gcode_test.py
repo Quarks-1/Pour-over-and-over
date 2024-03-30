be33d5755a41c5f65deea3881ffbb91dc5e54a1e
Quarks01 <riopacheco7@gmail.com>
2024-03-30 15:38:12 -0400

    modify test files

--- test_scripts/lib_gcode_test.py
+++ test_scripts/lib_gcode_test.py
@@ -37,8 +37,8 @@ while True:
     ser.write(str.encode(input_command + "\r\n"))
     time.sleep(2)
     cc=str(ser.readline())
-        while cc == b'echo:busy: processing\n' or cc == b'ok\n':
-            cc=str(ser.readline())
+    while cc == b'echo:busy: processing\n' or cc == b'ok\n':
+        cc=str(ser.readline())
     print(cc)
     
 
