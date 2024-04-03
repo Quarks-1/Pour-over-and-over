a2c2b294d5dab1ecf4d1b30ac3da6d2b1d3067ba
Quarks01 <riopacheco7@gmail.com>
2024-04-03 10:26:30 -0400

    bugfix: live data feed

--- pourover/static/js/brew_page.js
+++ pourover/static/js/brew_page.js
@@ -39,7 +39,7 @@ function connectToServer() {
         if (Array.isArray(response)) {
             updateParams(response)
         } 
-        else if (typeof(response) == String && "start data feed" in response) {
+        else if (response.includes("start data feed")) {
             setInterval(() => {
                 socket.send(JSON.stringify({"command": "updateData"}));
             }, 300);
