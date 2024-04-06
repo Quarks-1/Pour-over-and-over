842ce895569c6adfaea87ddcf88a3d84df9376ee
Quarks01 <riopacheco7@gmail.com>
2024-04-06 16:41:39 -0400

    pid script modification

--- test_scripts/pid_tuning.py
+++ test_scripts/pid_tuning.py
@@ -7,7 +7,7 @@ arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
 time.sleep(2)  # Wait for the serial connection to initialize
 print('Connected')
 # PID controller setup
-pid = PID(40, 4, 500, setpoint=200)  # P=1.0, I=0.1, D=0.05, desired temperature=25°C
+pid = PID(40, 5, 500, setpoint=200)  # P=1.0, I=0.1, D=0.05, desired temperature=25°C
 pid.sample_time = 0.5  # Update every 1 second
 pid.output_limits = (0, 1)  # Output value will be between 0 and 1 (off/on)
 
