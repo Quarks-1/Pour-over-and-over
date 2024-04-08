95fb02ba466bb60650ff13f6c6a5fae59df98804
Quarks01 <riopacheco7@gmail.com>
2024-04-08 11:23:13 -0400

    parse steps improvements

--- pourover/consumers.py
+++ pourover/consumers.py
@@ -225,6 +225,7 @@ class MyConsumer(WebsocketConsumer):
     def startBrew(self):
         while True:
             # Check if current time is time for next step
+            print(self.gcodeSteps)
             if datetime.now() >= self.gcodeSteps[0][1]:
                 print(f'Working on command: {self.gcodeSteps[0][0]}')
                 if 'Draw down' in self.gcodeSteps[0][0]:
