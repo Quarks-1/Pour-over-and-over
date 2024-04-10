cd6f65c38334d205c4851e4092f4ebdf035bfbd9
Quarks01 <riopacheco7@gmail.com>
2024-04-10 12:02:23 -0400

    fix sleep

--- pourover/consumers.py
+++ pourover/consumers.py
@@ -167,7 +167,7 @@ class MyConsumer(WebsocketConsumer):
     
     def get_arduino_feed(self):
         self.arduino.reset_input_buffer()
-        time.sleep(0.05)
+        time.sleep(0.1)
         decoded_str = ''
         while decoded_str == '':
             try:
