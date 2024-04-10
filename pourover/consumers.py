736c60ca19c6d77b4e712b4415382994ffc270af
Quarks01 <riopacheco7@gmail.com>
2024-04-10 11:54:14 -0400

    fix sleep

--- pourover/consumers.py
+++ pourover/consumers.py
@@ -175,7 +175,7 @@ class MyConsumer(WebsocketConsumer):
             except serial.SerialException:
                 continue
             decoded_str = data.decode('utf-8')
-            if decoded_str.split('/') == 2:
+            if len(decoded_str.split('/')) == 2:
                 break
             time.sleep(0.05)
         # Strip whitespace and newlines
