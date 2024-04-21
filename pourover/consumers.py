68bf3588bd23220364622142e0aad587260362fd
Quarks01 <riopacheco7@gmail.com>
2024-04-21 17:55:36 -0400

    add pour debug print

--- pourover/consumers.py
+++ pourover/consumers.py
@@ -265,8 +265,8 @@ class MyConsumer(WebsocketConsumer):
         times_dict = {
             'Center': 1,
             'Inner circle': 3.68,
-            'Outer circle': 11,
-            'Edge': 15, 
+            'Outer circle': 8,
+            'Edge': 11, 
         }
         gCode = {
             'pre_wet': 'G2 X127 Y115 Z220 I25 J25 F3600', 
