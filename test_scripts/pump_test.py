4498fabffed9042151fa87b202112e546068fd63
Quarks01 <riopacheco7@gmail.com>
2024-04-21 21:09:05 -0400

    add pour debug print

--- test_scripts/pump_test.py
+++ test_scripts/pump_test.py
@@ -1,6 +1,5 @@
 # Importing Libraries 
 import serial 
-import time 
 arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1) 
 
 while True:
@@ -10,6 +9,8 @@ while True:
         command = 'pumpon/255'
     elif command == 'off':
         command = 'pumpoff'
+    elif command == '':
+        continue
     else:
         command = f'pumpon/{command}'
     # send command to arduino
