80d01a9847dd9c71b16d0f850e820215c6a27094
Quarks01 <riopacheco7@gmail.com>
2024-04-26 19:31:09 -0400

    pump signal

--- pourover/consumers.py
+++ pourover/consumers.py
@@ -299,7 +299,7 @@ class MyConsumer(WebsocketConsumer):
     def doPour(self, water_weight, flowRate):
         # Prime
         self.arduino.write(b'pumpon/255\n')
-        time.sleep(1)
+        time.sleep(10)
         # Send signal to arduino
         print(f'Pouring {water_weight}g at {flowRate}g/s, value: {self.map_value(flowRate)}')
         # message = f'pumpon/{self.map_value(flowRate)}\n'
